7f55913597ac41e5e4b1d4a2f8a39c0e017b2b0a
Mikael Matveinen <mikael.matveinen@gmail.com>
2017-09-25 23:12:00 +0300

    Added snake graphics

---
+++ Snake/Assets/Game/Scripts/Game/Definitions/SnakeVisualDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Definitions
+{
+    [CreateAssetMenu]
+    public class SnakeVisualDefinition : ScriptableObject
+    {
+        public Sprite straightLeft;
+        public Sprite straightRight;
+        public Sprite straightUp;
+        public Sprite straightDown;
+
+        public Sprite upToRight;
+        public Sprite upToLeft;
+        public Sprite downToRight;
+        public Sprite downToLeft;
+
+        public Sprite leftToUp;
+        public Sprite leftToDown;
+        public Sprite rightToUp;
+        public Sprite rightToDown;
+
+        public Sprite headLeft;
+        public Sprite headRight;
+        public Sprite headUp;
+        public Sprite headDown;
+
+        public Sprite tailLeft;
+        public Sprite tailRight;
+        public Sprite tailUp;
+        public Sprite tailDown;
+    }
+}
